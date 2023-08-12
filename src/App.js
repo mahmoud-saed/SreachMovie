26a2012d2466f006ccedb6fab7b74634dbe74798
unknown <m.kotta00@gmial.com>
2023-08-12 15:57:08 +0300

    V1

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, {useEffect, useState} from 'react'
+import './App.css'
+import MovieCard from './MovieCard'
+import SearchIcon from './sreach.svg'
+
+
+// cc054119
+
+const API_URL = 'https://www.omdbapi.com?apikey=cc054119'
+
+const App = () => {
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (title) => { 
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json()
+
+    setMovies(data.Search);
+  }
+
+  useEffect(() =>{
+    searchMovies('Batman')
+  }, [])
+
+  return (
+    <div className="app">
+      <h1>Lights, Camera, Search: Find Your Perfect Movie!    .</h1>
+
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for movies"
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
